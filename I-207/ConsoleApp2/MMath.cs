2a9840c9bf2c50d7e2c39e1b550605bb1db6c0aa
412zTEDDYz <belka9870@gmail.com>
2026-09-09 11:21:22 +0500

    Task 1
    Работа на уроке Беляев Артём

---
+++ I-207/ConsoleApp2/MMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I207
+{
+    public class MMath:Math
+    {
+        void test()
+        {
+            sum(5, 6);
+        }
+    }
+}
---
+++ I-207/lesson2/HMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    internal class HMath:I207.MMath
+    {
+        void test()
+        {
+            sum(3, 7);
+        }
+    }
+}
