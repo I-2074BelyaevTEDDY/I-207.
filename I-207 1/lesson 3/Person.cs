ae782013e86ca18688afae09c0129d9c39fc1c26
I-2074BelyaevTEDDY <belka9870@gmail.ru>
2026-09-16 10:54:33 +0500

    task 3

---
+++ I-207 1/lesson 3/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson3
+{
+    internal class person
+    {
+        public int Id = 123;
+        public string name = "Artyom";
+        public string surName = "Belyaev";
+        public string familyName = "Aleksandrovich";
+        public DateTime birthDate = DateTime.Parse("14.10.2006");
+        public bool Gender = true;
+    }
+}
---
+++ I-207 1/lesson 3/Programm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson3
+{
+    internal class Programm
+    {
+        static lesson3.person clsperson = new lesson3.  person();
+        static void Main()
+        {
+            Console.WriteLine($"Id: {clsperson.Id}\n"+
+                $"FullName: {clsperson.name} {clsperson.surName} {clsperson.familyName}\n"+
+                $"BirthDate: {clsperson.birthDate}\n"+
+                $"Age: {DateTime.Now.Year - clsperson.birthDate.Year}\n"+
+                $"Gender: {(clsperson.Gender==true?"Man":"Girl")}");
+           
+        }
+    }
+}
